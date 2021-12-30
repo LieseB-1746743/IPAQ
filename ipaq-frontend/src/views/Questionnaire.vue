51d1c044d1ab48b617d20aea01df56dc0b28b2ae
JonasL-1746710 <jonas.leuckx@student.uhasselt.be>
2021-12-30 14:33:37 +0100

    Updated FAQ (tips)

--- ipaq-frontend/src/views/Questionnaire.vue
+++ ipaq-frontend/src/views/Questionnaire.vue
@@ -13,7 +13,7 @@
     <b-col sm="auto" align-self="end" >
       <!-- HELP :p krijg em nie links -->
       <b-button align-self="start" align-h="start" v-if="submit" id="change-btn" variant="outline-secondary" size="lg" v-on:click="ChangeBtnClicked" >Change</b-button>
-      <b-button v-if="!submit" id="next-btn" variant="outline-primary" size="lg" v-on:click="NextBtnClicked" >Next</b-button>
+      <b-button v-if="!submit" id="next-btn" variant="outline-primary" size="lg" v-on:click="NextBtnClicked"  v-on:keyup.enter="NextBtnClicked">Next</b-button>
       <b-button v-if="submit" id="submit-btn" variant="outline-primary" size="lg" v-on:click="SubmitBtnClicked" >Submit</b-button>
     </b-col>   
   </b-row>
@@ -36,6 +36,7 @@ import router from '../router';
     Chatbot
   },
 })
+
 
 export default class Questionnaire extends Vue {
   // Data ----------------------------------------------------------
