8bf6370db1e468d3e02ab1f69cf3437d408dca1d
Liese Bekkers <liese.bekkers@student.uhasselt.be>
2022-01-06 15:23:59 +0100

    Disabled next button when input is invalid

--- ipaq-frontend/src/views/Questionnaire.vue
+++ ipaq-frontend/src/views/Questionnaire.vue
@@ -13,7 +13,8 @@
     <b-col sm="auto" align-self="end" >
       <!-- HELP :p krijg em nie links -->
       <b-button align-self="start" align-h="start" v-if="submit" id="change-btn" variant="outline-secondary" size="lg" v-on:click="ChangeBtnClicked" >Change</b-button>
-      <b-button v-if="!submit" id="next-btn" variant="outline-primary" size="lg" v-on:click="NextBtnClicked">Next</b-button>
+      <b-button v-if="!submit && pageIsValidated " id="next-btn" variant="outline-primary" size="lg" v-on:click="NextBtnClicked">Next</b-button>
+      <b-button v-if="!submit && !pageIsValidated " id="next-btn" variant="outline-secondary" size="lg" v-on:click="NextBtnClicked" disabled>Next</b-button>
       <b-button v-if="submit" id="submit-btn" variant="outline-primary" size="lg" v-on:click="SubmitBtnClicked" >Submit</b-button>
     </b-col>   
   </b-row>
